20584667855ffc56075650b9c4866980d588e002
Justas Lapiene <justas63@gmail.com>
2022-11-22 12:36:16 +0100

    got the forms to book cards logic working

--- script.js
+++ script.js
@@ -17,31 +17,74 @@ function closeForm() {
     form.style.display = "none";
 }
 
-function createBook(title, author, yearOfPub, edition, publisher, placeOfPub) {
-    this.title = title
-    this.author = author
-    this.yearOfPub = yearOfPub
-    this.edition = edition
-    this.publisher = publisher
-    this.placeOfPub = placeOfPub
+function createBook(inputTitle, inputAuthor, inputYearOfPub, inputEdition, inputPublisher, inputPlaceOfPub) {
+    this.title = inputTitle
+    this.author = inputAuthor
+    this.yearOfPub = inputYearOfPub
+    this.edition = inputEdition
+    this.publisher = inputPublisher
+    this.placeOfPub = inputPlaceOfPub
 }
 
 function getInfo () {
-    const author = document.getElementById('author').value
-    const yearOfPub = document.getElementById('yearOfPublication').value
-    const title = document.getElementById('title').value
-    const edition = document.getElementById('edition').value
-    const publisher = document.getElementById('publisher').value
-    const placeOfPub = document.getElementById('placeOfPublication').value
+    const inputTitle = document.getElementById('title').value
+    const inputAuthor = document.getElementById('author').value
+    const inputYearOfPub = document.getElementById('yearOfPublication').value
+    const inputEdition = document.getElementById('edition').value
+    const inputPublisher = document.getElementById('publisher').value
+    const inputPlaceOfPub = document.getElementById('placeOfPublication').value
 
-    const book = new createBook(title, author, yearOfPub, edition, publisher, placeOfPub)
+    const newBook = new createBook(inputTitle, inputAuthor, inputYearOfPub, inputEdition, inputPublisher, inputPlaceOfPub)
 
-    addBookToLibrary(book)
-    for (let i = 0; i < myLibrary.length; i ++) {
-        console.log(myLibrary[i])
+    addBookToLibrary(newBook)
+    
+    console.log(myLibrary)
+
+    const bookLink = document.createElement('a')
+    bookLink.setAttribute('onclick', `event.preventDefault();`)
+    bookLink.setAttribute('href', "")
+
+    const book = document.createElement('div')
+    book.setAttribute('class', 'book')
+
+    const title = document.createElement('div')
+    title.setAttribute('class','title')
+    title.textContent = myLibrary[myLibrary.length -1].title
+
+    const author = document.createElement('div')
+    author.setAttribute('class','author')
+    author.textContent = myLibrary[myLibrary.length -1].author
+
+    const yearOfPub = document.createElement('div')
+    yearOfPub.setAttribute('class','yearOfPublication')
+    yearOfPub.textContent = myLibrary[myLibrary.length -1].yearOfPub
+
+    const edition = document.createElement('div')
+    edition.setAttribute('class','edition')
+    title.textContent = myLibrary[myLibrary.length -1].title
+
+    const publisher = document.createElement('div')
+    publisher.setAttribute('class','publisher')
+    edition.textContent = myLibrary[myLibrary.length -1].edition
+
+    const placeOfPub = document.createElement('div')
+    placeOfPub.setAttribute('class','placeOfPublication')
+    placeOfPub.textContent = myLibrary[myLibrary.length -1].placeOfPub
+
+    book.appendChild(title)
+    book.appendChild(author)
+    book.appendChild(yearOfPub)
+    book.appendChild(edition)
+    book.appendChild(publisher)
+    book.appendChild(placeOfPub)
+
+    bookLink.appendChild(book)
+
+    main.appendChild(bookLink)
     }
-    document.getElementById("arrPrint").innerHTML = JSON.stringify(myLibrary, null, 2);
-}
+
+    
+
 
    
 
